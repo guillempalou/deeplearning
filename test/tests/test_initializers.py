7db0b86c77e7a25cd583dbccc9d46bddcee72d2a
Guillem Palou Visa <gpalou@palantir.com>
2015-11-07 19:15:16 +0100

    Preliminary Softmax layer

--- test/tests/test_initializers.py
+++ test/tests/test_initializers.py
@@ -1,2 +1,17 @@
+import numpy as np
+from numpy.testing import assert_array_less, assert_almost_equal
+import scipy.stats as scs
+
+from deep_learning.initialization.constant_initializer import ConstantInitializer
+from deep_learning.initialization.random_initializers import RandomInitializer
+
+
 def test_constant():
-    ConstantInitializer()
+    ci = ConstantInitializer("constant", shape=1, value=2)
+    assert_almost_equal([2], ci().get_value())
+
+def test_random():
+    ri = RandomInitializer("uniform", d, distribution=scs.uniform(0, 1))
+    v = ri()
+    assert_array_less(v.get_value(), [1])
+    assert_array_less([0], v.get_value())
--- test/tests/test_layers.py
+++ test/tests/test_layers.py
@@ -1,6 +1,31 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+from deep_learning.initialization.constant_initializer import ConstantInitializer
+from deep_learning.layers.softmax import SoftMaxLayer
+from numpy.testing import assert_allclose
+
 
 def test_base():
     pass
 
+
 def test_softmax():
-    pass
+    parameters = {
+        "name": "test_softmax",
+        "in_shape": 3,
+        "out_shape": 2,
+        #TODO change the constructor parameters of initializers to make it easy
+        "initializer": {"w": ConstantInitializer(.1), "b": ConstantInitializer("b", 2., 1.)}
+    }
+
+    sl = SoftMaxLayer(**parameters)
+
+    # set up the vectors
+    X = T.dvector('X')
+    f = theano.function([X], sl.transform(X))
+    a = f([1, 2, 3])
+
+    # since the coefficients are the same, the softmax retuns 0.5,0.5
+    assert_allclose(a, np.array([[0.5, 0.5]]))
