1c83bfc6fccf993fe34f5a14538675c9e0c7ac40
Guillem Palou Visa <gpalou@palantir.com>
2016-01-16 22:29:09 +0000

    Added factory docstrings

--- src/deep_learning/factories/initializer_factory.py
+++ src/deep_learning/factories/initializer_factory.py
@@ -11,24 +11,56 @@ class ParametersInitializers:
     RandomInitializer = "random"
     FanInOutInitializer = "faninout"
 
+
 def create_constant_initializer(name, shape, **kwargs):
+    """
+    Creates a constant initializer for theano shared variables
+    :param name: name of the variable
+    :param shape: shape
+    :param kwargs: additional arguments
+    :return:
+    """
     return ConstantInitializer(name, shape, kwargs["value"])
 
 
 def create_random_initializer(name, shape, **kwargs):
+    """
+    Creates a random initializer for theano shared variables
+    :param name: name of the variable
+    :param shape: shape
+    :param kwargs: additional arguments
+    :return:
+    """
     return RandomInitializer(name, shape, kwargs["distribution"])
 
 
 def create_faninout_initializer(name, shape, **kwargs):
+    """
+    Creates a random initializer for theano shared variables with Fan In Out formula
+    :param name: name of the variable
+    :param shape: shape
+    :param kwargs: additional arguments
+    :return:
+    """
     return FanInOutInitializer(name, shape)
 
+
+# map that translates the type of initializer to the function that needs to be called
 param_to_init = {
     ParametersInitializers.ConstantInitializer: create_constant_initializer,
     ParametersInitializers.RandomInitializer: create_random_initializer,
     ParametersInitializers.FanInOutInitializer: create_faninout_initializer
 }
 
+
 def create_initializer(name, shape, **kwargs):
+    """
+    Generic functions that creates an initializer based on shape and types
+    :param name: name
+    :param shape: shape
+    :param kwargs: additional arguments
+    :return: initializer
+    """
     if isinstance(shape, int):
         shape = np.array((shape,))
     elif isinstance(shape, np.ndarray):
@@ -42,4 +74,3 @@ def create_initializer(name, shape, **kwargs):
     assert "initializer" in kwargs, "You need an 'initializer' in the parameters"
 
     return param_to_init[kwargs["initializer"]](name, shape, **kwargs)
-
